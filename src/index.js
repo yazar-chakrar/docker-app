416c26ec6698a317773603bd449e9c9b7b470cb9
Mohammed El Amin CHAKRAR <37815501+yazar-chakrar@users.noreply.github.com>
2022-11-11 12:22:01 +0100

    Refactoring-src-app

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT=4000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world, its YAZAR! .YY..  VV</h1>')
+})
+
+app.listen(
+    PORT,
+    () => console.log(`app is running on port ${PORT}`)
+);
